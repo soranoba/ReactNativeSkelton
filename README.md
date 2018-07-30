0839bc7e7354c8727efdfaf76185204929b33bf0
soranoba <soranoba@gmail.com>
2018-07-31 00:43:21 +0900

    support GNU sed

--- README.md
+++ README.md
@@ -12,16 +12,12 @@ Because CreateReactNativeApp is not maintained.
 
 ## Usage
 
-for OSX
-
 ```bash
-git clone https://github.com/soranoba/ReactNativeSkelton #{YOUR_PROJECT_NAME}
-cd #{YOUR_PROJECT_NAME}
+git clone https://github.com/soranoba/ReactNativeSkelton ${YOUR_PROJECT_NAME}
+cd ${YOUR_PROJECT_NAME}
 rm -rf .git
-sh scripts/renameProject.sh #{YOUR_PROJECT_NAME}
+sh scripts/renameProject.sh ${YOUR_PROJECT_NAME}
 ```
-
-If you want to run on Linux, `sed` may not work properly.
 
 ## CI
 
--- scripts/renameProject.sh
+++ scripts/renameProject.sh
@@ -5,18 +5,22 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
+SED="sed"
 DST_NAME=${1}
 SRC_NAME="ReactNativeSkelton"
 
 for SRC_FILE in $(find . -name ${SRC_NAME}* -type d); do
-  DST_FILE=$(echo ${SRC_FILE} | sed "s|${SRC_NAME}|${DST_NAME}|g")
+  DST_FILE=$(echo ${SRC_FILE} | ${SED} "s|${SRC_NAME}|${DST_NAME}|g")
   mv ${SRC_FILE} ${DST_FILE}
 done
 for SRC_FILE in $(find . -name ${SRC_NAME}* -type f); do
-  DST_FILE=$(echo ${SRC_FILE} | sed "s|${SRC_NAME}|${DST_NAME}|g")
+  DST_FILE=$(echo ${SRC_FILE} | ${SED} "s|${SRC_NAME}|${DST_NAME}|g")
   mv ${SRC_FILE} ${DST_FILE}
 done
 
 export LANG=C
-grep --exclude "*/node_modules/*" --exclude "*/vendor/*" --exclude "*/.git/*" -r ${SRC_NAME} -l $(dirname $0)/.. | \
-    xargs sed -i '' "s|${SRC_NAME}|${DST_NAME}|g"
+for TARGET_FILE in $(grep --exclude "*/node_modules/*" --exclude "*/vendor/*" --exclude "*/.git/*" -r ${SRC_NAME} -l $(dirname $0)/..); do
+  DUMP="$(cat ${TARGET_FILE} | ${SED} "s|${SRC_NAME}|${DST_NAME}|g")"
+  rm ${TARGET_FILE}
+  echo "${DUMP}" > ${TARGET_FILE}
+done
